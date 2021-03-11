58f4ef714628e22d3941c8c49b158d76724d202d
Hugo Susumu <hugo.asaga@usp.br>
2021-03-11 17:12:22 -0300

    first commit

---
+++ Dungeon Crawler/Assets/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+    public int maxMana;
+    public int currentMana;
+
+}
