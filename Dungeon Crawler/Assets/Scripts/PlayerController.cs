6fc1d17e01c1cf723ffab01448f90fd129ab227d
Susumu314 <48767947+Susumu314@users.noreply.github.com>
2021-04-01 18:15:29 -0300

    Testes com criação de dungeons, melhorias na movimentacao do jogador

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-    public float step = 3f;
+    public float moveSpeed = 16f;
+    public float step = 4f;
     public float rotationSpeed = 360f;
     public Transform movePoint;
+    public Vector3 Target;
 
     private List<Input> inputBuffer = new List<Input>();
 
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         movePoint.parent = null;
+        CollisionMask = (1<<8);//wall
     }
 
     // Update is called once per frame
@@ -25,15 +27,25 @@ public class PlayerController : MonoBehaviour
         
         transform.rotation =  Quaternion.RotateTowards(transform.rotation, movePoint.rotation, rotationSpeed * Time.deltaTime);
         
-        //modificar isso aqui para funcionar com buffer de input
+        //modificar isso aqui para funcionar com buffer de input para o movimento ser mais fluido
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.1f*step && Quaternion.Angle(transform.rotation, movePoint.rotation) <= 0.1f){
             if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f){
                 movePoint.Rotate(0f, 90f * Input.GetAxisRaw("Horizontal"), 0f);
             }
             else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
-                movePoint.position += transform.rotation * Vector3.forward * Input.GetAxisRaw("Vertical")*step;
+                Target = movePoint.position + transform.rotation * Vector3.forward * Input.GetAxisRaw("Vertical")*step;
+                if(!Physics.Linecast(transform.position, Target , CollisionMask))//check if there is a wall
+                {
+                    movePoint.position += transform.rotation * Vector3.forward * Input.GetAxisRaw("Vertical")*step;
+                }
             }
-            //adicionar strafe
+            else if(Mathf.Abs(Input.GetAxisRaw("Strafe")) == 1f){
+                Target = movePoint.position + transform.rotation * Vector3.right * Input.GetAxisRaw("Strafe")*step;
+                if(!Physics.Linecast(transform.position, Target , CollisionMask))//check if there is a wall
+                {
+                    movePoint.position += transform.rotation * Vector3.right * Input.GetAxisRaw("Strafe")*step;
+                }
+            }
         }
     }
 }
